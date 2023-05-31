46cf9c32fab293f5c7be134f8f0a54c80535aac1
EdinaelsonGaleno <sedinaelson@gmail.com>
2023-05-30 21:55:00 -0300

    exception

--- Teste.java
+++ Teste.java
@@ -6,32 +6,32 @@ public class Teste {
     public ArrayList<Circulo> listaDeCirculos = new ArrayList<>();
 
     void criarRetangulo(float lado, float altura) {
-       
 
         if(listaDeQuadrilateros.size() >= 5) {
-            System.out.println("Limite da lista atigindo");
+            throw new IndexOutOfBoundsException("Limite atigindo");
         } else if (lado <= 0 || altura <= 0) {
-            throw new ArithmeticException
-            ("O valor não podem ser igual ou menor que 0");  
+            throw new ArithmeticException("O valor não podem ser igual ou menor que 0");
         }else {
             Retangulo retangulo = new Retangulo();
             retangulo.setLado(lado);
             retangulo.setAltura(altura);
             listaDeQuadrilateros.add(retangulo);
         }
+
     }
 
     void criarQuadrado(float lado) {
+
         if(listaDeQuadrilateros.size() >= 5) {
-            System.out.println("Limite da lista atigindo");
+            throw new IndexOutOfBoundsException("Limite atigindo");
         } else if (lado <= 0) {
-            throw new ArithmeticException
-            ("O valor não podem ser igual ou menor que 0");  
+            throw new ArithmeticException("O valor não podem ser igual ou menor que 0");
         }else {
             Quadrado quadrado = new Quadrado();
             quadrado.setLado(lado);
             listaDeQuadrilateros.add(quadrado);
         }
+
     }
 
     void criarCirculo(float raio) {
@@ -39,30 +39,28 @@ public class Teste {
             Circulo circulo = new Circulo();
 
             if (raio <= 0) {
-                throw new ArithmeticException
-                ("O valor não podem ser igual ou menor que 0");  
+                throw new ArithmeticException("O valor não podem ser igual ou menor que 0");
             } else {
                 if(listaDeQuadrilateros.size() >= 5) {
-                    System.out.println("Limite da lista de quadrilateros atigindo");
+                    throw new IndexOutOfBoundsException("Limite atigindo");
                 } else {
                     circulo.setRaio(raio);
                     listaDeQuadrilateros.add(circulo);   
                 }
                 listaDeCirculos.add(circulo);  
             }
-            
-        
     }
 
     public static void main(String[] args) {
        Teste teste = new Teste();
+
        teste.criarRetangulo(7,3);
        teste.criarRetangulo(3,4);
-    
        teste.criarQuadrado(5);
+
        teste.criarCirculo(10);
-       
-      
+       teste.criarCirculo(10);
+
        teste.listaDeQuadrilateros.get(1).calcularPerimetro();
        teste.listaDeQuadrilateros.get(1).calcularArea();
        teste.listaDeQuadrilateros.get(2).calcularArea();
@@ -70,5 +68,7 @@ public class Teste {
        
        teste.listaDeCirculos.get(0).calcularArea();
        teste.listaDeCirculos.get(0).calcularPerimetro();
+
+
     }
 }
